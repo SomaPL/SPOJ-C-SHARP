91baa84e35cf05bf9595c8415fd0aa1739e5599a
Jakub Kot <Jakub.Kot@microsoft.wsei.edu.pl>
2022-03-13 04:24:11 +0100

    ADD

---
+++ NWD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NWD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = int.Parse(Console.ReadLine());
+            for (int i = 0; i < test; i++)
+            {
+                string[] liczby = Console.ReadLine().Split(' ');
+                int a = int.Parse(liczby[0]);
+                int b = int.Parse(liczby[1]);
+                Console.WriteLine(nwd(a, b));
+            }
+        }
+        static int nwd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int c = a % b;
+                a = b;
+                b = c;
+            }
+            return a;
+        }
+    }
+}
